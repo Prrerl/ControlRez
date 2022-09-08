b3d9d0c751319170e5d6286fcd3fd2a986366cdd
Atlas <Onix.Darwater@yandex.ru>
2022-09-08 11:55:14 +0300

    1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Сколько слов вы собираетесь ввести? ");
+            int val = Convert.ToInt32(Console.ReadLine());
+            string[] arr1 = new string[val];
+            int cntr = 0;
+            int condCntr = 0;
+            Console.WriteLine("Введите слова ");
+            while (cntr < val)
